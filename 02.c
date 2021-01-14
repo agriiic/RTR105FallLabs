ea70569554fd409e5f544f43560b6c99bc948aaf
agriiic <73386238+agriiic@users.noreply.github.com>
2021-01-14 18:01:13 +0200

    Create 02.c

---
+++ 02.c
@@ -0,0 +1,22 @@
+/* 02.c Programma ar datu izvadi (data out)
+Programmas autors: Agris Lapins
+Mana pirma programma. Veidota 2021. gada 11. janvari
+Versija: 0.0.1
+Programma Mani sveicina */
+
+#include<stdio.h> //Te tiek ieklauta izvades funkciju biblioteka
+
+int main()
+{
+        printf("Sveiks, Agri!\n");
+        printf("Sveiks, Agri!\r");
+        printf("Sveiks, Agri\t");
+        printf("Sveiks, Agri!\v");
+        printf("Sveiks, Agri!\b");
+        printf("Sveiks, Agri!\f");
+        printf("Sveiks, Agri!\a");
+        printf("Sveiks, Agri!\'");
+        printf("Sveiks, Agri!\"");
+        printf("Sveiks, Agri!\?");
+        printf("Sveiks, Agri!\\");
+}
